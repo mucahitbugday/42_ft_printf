376369a415171db2106e14a29332ad0600220bfe
mücahit buğday <mchtbgd@gmail.com>
2022-03-27 02:43:45 +0300

    ft_print.h dosyası eklendi

---
+++ inc/ft_printf.h
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mbugday <mbugday@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/21 14:37:39 by mbugday           #+#    #+#             */
+/*   Updated: 2022/03/21 18:29:50 by mbugday          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include <stdlib.h>
+# include <stdarg.h>
+# include <unistd.h>
+
+int		ft_printf(const char *str, ...);
+int		ft_formats(va_list args, const char format);
+int		ft_print_char(int c);
+int		ft_print_str(char *str);
+int		ft_print_ptr(unsigned long long ptr);
+int		ft_print_nbr(int n);
+int		ft_print_unsigned(unsigned int n);
+int		ft_print_hex(unsigned int num, const char format);
+int		ft_print_percent(void);
+char	*ft_itoa(int n);
+void	ft_putchar_fd(char c, int fd);
+
+#endif
